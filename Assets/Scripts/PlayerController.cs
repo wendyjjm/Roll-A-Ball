9ae5e6ff428c558b47765324840a30a361342285
wendyjjm <wendyjjm0628@gmail.com>
2016-02-22 15:27:57 -0800

    First Unity 3D Game

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	private Rigidbody rb;
+
+	public float speed;
+	private int count;
+
+	public Text countText;
+	public Text winText;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+		count = 0;
+		SetCountText();
+		winText.text = "";
+	}
+
+
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal,0.0f,moveVertical);
+
+		rb.AddForce(movement*speed);
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag ("PickUp")) 
+		{
+			other.gameObject.SetActive(false);
+			count = count + 1;
+			SetCountText();
+		}
+	}
+
+	void SetCountText()
+	{
+		countText.text = "Count :" + count.ToString ();
+		if (count == 12) 
+		{
+			winText.text ="You Win!";
+		}
+	}
+}
